87a0296c032643ec25a12c3023ebed090c7f1eb3
arafat-alim <arafat97alim@gmail.com>
2022-09-06 17:19:47 +0530

    object length

---
+++ Objects/obj-length.js
@@ -0,0 +1,18 @@
+// ! Array length
+
+let arr = ["1", "4", "3", "2"];
+console.log(arr.length); //4
+
+//! object length
+let obj1 = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 6,
+  d: 6,
+};
+
+//turn object into an array -- arrKey
+let arrKey = Object.keys(obj1).length;
+console.log(arrKey); //5
