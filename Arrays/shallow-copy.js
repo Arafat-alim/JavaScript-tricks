280222c7e363da630bf462e82785bd6671af1cfe
arafat-alim <arafat97alim@gmail.com>
2022-09-07 16:04:21 +0530

    shallow copy - CLone Array.

---
+++ Arrays/shallow-copy.js
@@ -0,0 +1,25 @@
+//! Shallow Copy Clone Array
+let arr = ["red", "blue", "green"];
+
+let newArr = arr;
+
+newArr.push("Black");
+console.log(newArr); //[ 'red', 'blue', 'green', 'Black' ]
+console.log(arr); //[ 'red', 'blue', 'green', 'Black' ]
+
+//! The above program seem here that we are copying reference
+//! Slice Method
+let sliceCopy = arr.slice(0);
+sliceCopy.push("rainbow");
+console.log(sliceCopy); //[ 'red', 'blue', 'green', 'Black', 'rainbow' ]
+console.log(arr); //[ 'red', 'blue', 'green', 'Black' ]
+
+//! concat method
+let concatCopy = arr.concat("reddish");
+console.log(concatCopy); //[ 'red', 'blue', 'green', 'reddish' ]
+console.log(arr); //[ 'red', 'blue', 'green' ]
+
+//! Spread operator
+let spreadCopy = ["yello", ...arr, "pink"];
+console.log(spreadCopy); //[ 'yello', 'red', 'blue', 'green', 'pink' ]
+console.log(arr); //[ 'red', 'blue', 'green' ]
