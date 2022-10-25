eacbbac748720ea050057ef502bf9b324e510ea8
arafat-alim <arafat97alim@gmail.com>
2022-10-25 15:57:20 +0530

    wildcard period added one more quesiton

--- regex/06-wildcard-period-match.js
+++ regex/06-wildcard-period-match.js
@@ -8,3 +8,10 @@ let resultOne = humStr.match(pattern);
 let resulTwo = hugStr.match(pattern);
 console.log(resultOne); //[ 'hum', index: 5, input: 'I\'ll hum a song', groups: undefined ]
 console.log(resulTwo); // [ 'hug', index: 5, input: 'Bear hug', groups: undefined ]
+
+//! Question
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let result = unRegex.test(exampleStr);
+
+console.log(result); //true
