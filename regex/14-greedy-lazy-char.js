a8e1094d12542c9eca8c5a9205e2a0e51570386d
arafat-alim <arafat97alim@gmail.com>
2022-10-25 23:08:17 +0530

    greedy and lazy character

---
+++ regex/14-greedy-lazy-char.js
@@ -0,0 +1,20 @@
+//! By default regex is greedy character
+let string = "titanic";
+let pattern = /t[a-z]*i/i; //greedy approach
+let patternLazy = /t[a-z]*?i/;
+
+let resultOne = string.match(pattern);
+console.log(resultOne); //!  ['titani']
+
+let resultTwo = string.match(patternLazy);
+console.log(resultTwo); //! ['ti']
+
+let text = "<h1>Winter is coming</h1>";
+//! Greedy
+let regexOne = /<.*>/;
+let ansOne = text.match(regexOne);
+console.log(ansOne); //! ['<h1>Winter is coming</h1>']
+//! Lazy
+let regexTwo = /<.*?>/;
+let ansTwo = text.match(regexTwo);
+console.log(ansTwo); //! ['<h1>']
