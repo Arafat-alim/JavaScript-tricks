9649d8b848a7e5ec65a66f688e969e28e84ce11d
arafat-alim <arafat97alim@gmail.com>
2022-10-26 14:10:51 +0530

    capture group with replace method

--- regex/31-capture-group-replace.js
+++ regex/31-capture-group-replace.js
@@ -10,8 +10,13 @@ let inputStr = "Code Camp";
 //! using capture group
 let pattern = /(\w+)\s(\w+)/;
 //! using replace method
-let resultTwo = inputStr.replace(pattern, "$2 $1"); // strange one
+let resultTwo = inputStr.replace(pattern, "$2 $1"); //
+//! $1 --> represent the first arugment of capture group
+//! $2 --> Represeent the second argument of capture group
 console.log(resultTwo); //! Camp Code
+
+let testOne = "I love JavaScript".replace(/(\w+)\s(\w+)\s(\w+)/, "$3 $2 $1");
+console.log(testOne); //JavaScript love I
 
 //! Q.2
 let huhText = "This sandwich is good.";
